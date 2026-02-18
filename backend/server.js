34c2cf959f4fb318054174820ed667312037029d
sumukhsp <116740042+sumukhsp@users.noreply.github.com>
2026-02-18 10:06:56 +0530

    Authentication APIs added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req,res)=> res.send("API running"));
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
-app.listen(5000, ()=> console.log("Server running"));
+app.get("/", (req, res) => {
+  res.send("Habit Builder API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
